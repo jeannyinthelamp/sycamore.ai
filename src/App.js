734a68e02611b37e758f7611f16fdb51beda7ee2
Abrar <abrarmurad00@gmail.com>
2023-08-14 18:35:40 -0400

    added signing in to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Loggingin from "./Components/Login/Loggingin";
+import Signingin from "./Components/Signin/Signingin";
 
 export default function App() {
     return (
         <div>
             <Loggingin />
+            <Signingin />
         </div>
     );
 }
