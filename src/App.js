71b24c328254bbf5955fa135a26b5610b94429d6
Abrar <abrarmurad00@gmail.com>
2023-08-14 19:11:55 -0400

    tailwind successfully setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Loggingin from "./Components/Login/Loggingin";
 import Signingin from "./Components/Signin/Signingin";
+import "./index.css";
 
 export default function App() {
     return (
--- src/Components/Login/Loggingin.js
+++ src/Components/Login/Loggingin.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Loggingin = () => {
-  return (
-    <div>Loggingin</div>
-  )
-}
+    return <div>Loggingin</div>;
+};
 
-export default Loggingin
+export default Loggingin;
