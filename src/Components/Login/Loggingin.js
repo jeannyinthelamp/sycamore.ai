643ef4d50ba82206f9752c9cc03667a4f45d561e
Abrar <abrarmurad00@gmail.com>
2023-08-14 19:03:49 -0400

    temporary revert

--- src/Components/Login/Loggingin.js
+++ src/Components/Login/Loggingin.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Loggingin = () => {
-    return <div>Loggingin</div>;
+    return (
+        <div className="border-2 border-black p-4 text-red-400">
+            <div className="text-red-800 text">hello there</div>
+        </div>
+    );
 };
 
 export default Loggingin;
