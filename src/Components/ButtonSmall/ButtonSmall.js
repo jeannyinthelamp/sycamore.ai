2e943717d49026e59fbbfbf2ceda8ef6a391aaeb
Akin Evans <akinevans@gmail.com>
2023-08-15 13:02:34 -0700

    Build ButtonSmall Component,create Assets,a dn Icon folder, Import Google Logo svg

---
+++ src/Components/ButtonSmall/ButtonSmall.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function ButtonSmall(props) {
+  return (
+    <button
+      //props.className is for adding any additional styling thats needed when a ButtonSmall component is rendered
+      //! add Rubik Font
+      className={`${props.className} w-[283px] h-[43px] my-4 bg-base-black text-white font-Rubik text-center font-normal text-sm leading-[16.59px] cursor-pointer`}
+      onClick={props.onClick}
+    >
+      {props.btnText}
+    </button>
+  );
+}
