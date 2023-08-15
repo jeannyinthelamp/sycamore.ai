363eb1ae2d9c5058894e41cf2c6ed6171a86bfd8
Akin Evans <akinevans@gmail.com>
2023-08-15 10:23:25 -0700

    Create sign-up branch, create Signup.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Loggingin from "./Components/Login/Loggingin";
 import Signingin from "./Components/Signin/Signingin";
+import Signup from "./Components/Signup/Signup";
 import "./index.css";
 
 export default function App() {
-    return (
-        <div>
-            <Loggingin />
-            <Signingin />
-        </div>
-    );
+  return (
+    <div>
+      {/* <Loggingin /> */}
+      {/* <Signingin /> */}
+      <Signup />
+    </div>
+  );
 }
