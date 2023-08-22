08039f2142dd402430ba915f5b7f927fbe98b9ac
Akin Evans <akinevans@gmail.com>
2023-08-22 11:46:46 -0700

    Build page 2 of the signup process, component name  Signup2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Loggingin from "./Components/Login/Loggingin";
 import Signingin from "./Components/Signin/Signingin";
 import Signup from "./Components/Signup/Signup";
+import Signup2 from "./Components/Signup2/Signup2";
 import "./index.css";
 
 export default function App() {
@@ -9,7 +10,8 @@ export default function App() {
     <div>
       {/* <Loggingin /> */}
       {/* <Signingin /> */}
-      <Signup />
+      {/* <Signup /> */}
+      <Signup2 />
     </div>
   );
 }
