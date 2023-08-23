4744dfdca0174ee03594fdd18d71e354dc21169e
Akin Evans <akinevans@gmail.com>
2023-08-23 12:14:12 -0700

    Build 404 ErrorPage and add route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 import Loggingin from "./Pages/Login/Loggingin";
 import Signup from "./Pages/Signup/Signup";
 import Signup2 from "./Pages/Signup2/Signup2";
+import ErrorPage from "./Pages/ErrorPage/ErrorPage";
 import "./index.css";
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       <Route path='/' element={<Loggingin />} />
       <Route path='/sign-up' element={<Signup />} />
       <Route path='/sign-up-2' element={<Signup2 />} />
+      <Route path='*' element={<ErrorPage />} />
     </Routes>
   );
 }
---
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ErrorPage() {
+  return (
+    <div className='w-screen h-screen flex flex-col justify-start items-center'>
+      <h1 className='mt-7 text-[50px]'>404</h1>
+      <h2 className='mt-5 mb-5 text-[25px]'>Page not found</h2>
+    </div>
+  );
+}
