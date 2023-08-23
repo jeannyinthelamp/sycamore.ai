fe80e8846ec2e316a0839521ded5ab7e8c8b3917
Akin Evans <akinevans@gmail.com>
2023-08-23 11:07:14 -0700

    Relocate Signup2 component into pages folder, update component import statements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Loggingin from "./Pages/Login/Loggingin";
 import Signup from "./Pages/Signup/Signup";
+import Signup2 from "./Pages/Signup2/Signup2";
 import "./index.css";
 
 export default function App() {
-    return (
-        <div>
-            <Loggingin />
-            <Signup />
-        </div>
-    );
+  return (
+    <div>
+      <Loggingin />
+      <Signup />
+      <Signup2 />
+    </div>
+  );
 }
