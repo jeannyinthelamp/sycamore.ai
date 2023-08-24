3242f95c18b8224462327d018ccae06829769a53
Akin Evans <akinevans@gmail.com>
2023-08-24 13:45:00 -0700

    Create Landing.js file, Add Landing Route to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 
+import Landing from "./Pages/Landing/Landing";
 import Loggingin from "./Pages/Login/Loggingin";
 import Signup from "./Pages/Signup/Signup";
 import Signup2 from "./Pages/Signup2/Signup2";
@@ -11,7 +12,8 @@ import "./index.css";
 export default function App() {
   return (
     <Routes>
-      <Route path='/' element={<Loggingin />} />
+      <Route path='/' element={<Landing />} />
+      <Route path='/login' element={<Loggingin />} />
       <Route path='/sign-up' element={<Signup />} />
       <Route path='/sign-up-2' element={<Signup2 />} />
       <Route path='/onboarding_1' element={<Onboarding_1 />} />
