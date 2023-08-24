794da03374debaa86dafa062b73425393eced05f
Akin Evans <akinevans@gmail.com>
2023-08-24 11:44:51 -0700

    Build Onboarding_header and Onboarding_nav components

--- src/Pages/Onboarding/Onboarding_1.js
+++ src/Pages/Onboarding/Onboarding_1.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Onboarding_header from "../../Components/ButtonSmall/Onboarding_header/Onboarding_header";
+import Onboarding_nav from "../../Components/ButtonSmall/Onboarding_nav/Onboarding_nav";
 
 export default function Onboarding_1() {
   return (
     <div>
       <Onboarding_header />
-      {/* import onboarding nav */}
+      <Onboarding_nav />
     </div>
   );
 }
