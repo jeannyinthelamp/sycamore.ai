2786d3284c20fac90ae574f3785456e53bef39b7
Abrar <abrarmurad00@gmail.com>
2023-08-26 02:06:04 -0400

    landing page components - Abrar

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -1,5 +1,8 @@
 import React from "react";
+import Navbar from "../../Components/Navbar/Navbar";
+import Hero from "../../Components/Hero/Hero";
+import FAQ from "../../Components/FAQ/FAQ";
 
 export default function Landing() {
-  return <div>Landing</div>;
+    return <div>Landing</div>;
 }
