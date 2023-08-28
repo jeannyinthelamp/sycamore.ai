fa051a870f330bd855d8b7edf5936bb2ddd4ad6e
Akin Evans <akinevans@gmail.com>
2023-08-28 10:35:53 -0700

    Begin setting up the About Us, Footer, and Waitlist components for the landing page. Import icons and images to assets folder. Update file structure

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -1,5 +1,13 @@
 import React from "react";
+import Footer from "../../Components/Footer/Footer";
 
 export default function Landing() {
-  return <div>Landing</div>;
+  return (
+    <div>
+      Landing
+      {/* Import Waitlist */}
+      {/* Import About Us */}
+      <Footer />
+    </div>
+  );
 }
