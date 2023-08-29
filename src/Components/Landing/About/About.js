569cc2d781b22e0c10e208749d4d4bfca669c175
Akin Evans <akinevans@gmail.com>
2023-08-29 12:36:26 -0700

    Create About js file

---
+++ src/Components/Landing/About/About.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function About() {
+  return <section className='about-outer-wrapper'>About Us</section>;
+}
--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../../Components/Landing/Navbar/Navbar";
+import About from "../../Components/Landing/About/About";
 import Footer from "../../Components/Landing/Footer/Footer";
 
 export default function Landing() {
@@ -7,6 +8,7 @@ export default function Landing() {
     <div>
       Landing
       <Navbar />
+      <About />
       <Footer />
     </div>
   );
