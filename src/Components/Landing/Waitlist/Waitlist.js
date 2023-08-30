9298c6777bdae093317ff11e3b36b84036e2f20f
Akin Evans <akinevans@gmail.com>
2023-08-30 12:19:47 -0700

    Begin building Waitlist component for Landing page

---
+++ src/Components/Landing/Waitlist/Waitlist.js
@@ -0,0 +1,80 @@
+import React from "react";
+import smart_people_left from "../../../Assets/Images/landing-waitlist-img-1.svg";
+import smart_people_right from "../../../Assets/Images/landing-waitlist-img-2.svg";
+
+export default function Waitlist() {
+  return (
+    <section className='waitlist-outer-wrapper w-screen h-auto flex justify-center items-center bg-[#FFF]'>
+      <div className='waitlist-inner-wrapper relative w-[1440px] h-[800px] flex justify-center items-center'>
+        <div className='waitlist-card-wrapper z-10 w-[500px] h-auto flex flex-col justify-start items-center p-[50px] bg-white border border-solid border-[#CED4DA] rounded-[8px]'>
+          <div className='waitlist-card-text-wrapper w-[298px] h-auto flex flex-col gap-[16px] items-center'>
+            <h1 className='font-[Poppins] text-[28px] font-semibold leading-[42px]'>
+              Join our Waitlist
+            </h1>
+            <p className='font-[Inter] text-[14px] text-[#212529] font-normal leading-[21px] text-center'>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod
+            </p>
+          </div>
+          {/* //* form */}
+          <form className='waitlist-form-wrapper mt-[32px] flex flex-col justify-start items-center'>
+            <div className='first-last-names-wrapper flex flex-row'>
+              <label className='flex flex-col font-[Inter] text-[#6C757D] text-[14px] font-normal leading-[24px]'>
+                First Name
+                <input
+                  //
+                  type='text'
+                  name='first-name'
+                  className=' w-[191px] h-auto mt-[8px] mr-[17px] py-[10px] px-[16px] outline outline-1 outline-[#CED4DA] rounded-[8px]'
+                />
+              </label>
+              <label className='flex flex-col font-[Inter] text-[#6C757D] text-[14px] font-normal leading-[24px]'>
+                Last Name
+                <input
+                  //
+                  type='text'
+                  name='last-name'
+                  className='w-[191px] h-auto mt-[8px] py-[10px] px-[16px] outline outline-1 outline-[#CED4DA] rounded-[8px]'
+                />
+              </label>
+            </div>
+            {/* //* email to submit button wrapper */}
+            <div className='form-email-button-wrapper w-auto h-auto mt-[32px] flex flex-col justify-center items-center'>
+              <label className='flex flex-col font-[Inter] text-[#6C757D] text-[14px] font-normal leading-[24px]'>
+                Email
+                <input
+                  //
+                  type='email'
+                  name='email'
+                  required
+                  className=' w-[400px] h-auto mt-[8px] py-[10px] px-[16px] outline outline-1 outline-[#CED4DA] rounded-[8px]'
+                />
+              </label>
+              {/* //* Toggle here */}
+              <div className='toggle-wrapper mt-[33.5px] flex flex-row '></div>
+
+              <input
+                type='submit'
+                value='Join Waitlist'
+                className='w-[400px] mt-[32px] py-[12px] px-[24px] flex justify-center items-center bg-[#556AEB] rounded-lg font-[Poppins] text-[#FFF] text-center text-[16px] font-medium leading-[24px] outline-none border-none cursor-pointer'
+              />
+            </div>
+          </form>
+        </div>
+        {/* //* images here */}
+        <img
+          //
+          src={smart_people_left}
+          alt='floating business man'
+          className='absolute z-0 top-[25%] left-[17.6%]  w-[385px] h-[399px]'
+        />
+        <img
+          //
+          src={smart_people_right}
+          alt='artist painting'
+          className='absolute z-0 bottom-[22%] right-[20.4%] w-[209px] h-[242px]'
+        />
+      </div>
+    </section>
+  );
+}
--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Navbar from "../../Components/Landing/Navbar/Navbar";
 import Hero from "../../Components/Landing/Hero/Hero";
+import Waitlist from "../../Components/Landing/Waitlist/Waitlist";
 import About from "../../Components/Landing/About/About";
 import Footer from "../../Components/Landing/Footer/Footer";
 
 export default function Landing() {
-    return (
-        <div>
-            <Navbar />
-            <Hero />
-            <About />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      Landing
+      <Navbar />
+      {/* <Hero /> */}
+      <Waitlist />
+      <About />
+      <Footer />
+    </div>
+  );
 }
