09f1f8060465ec42c6bba43d68576ca0905dc7a5
Akin Evans <akinevans@gmail.com>
2023-08-31 12:05:51 -0700

    Update Waitlist component with toggle switch

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -2,17 +2,19 @@ import React from "react";
 import Navbar from "../../Components/Landing/Navbar/Navbar";
 import Hero from "../../Components/Landing/Hero/Hero";
 import FAQ from "../../Components/Landing/FAQ/FAQ";
+import Waitlist from "../../Components/Landing/Waitlist/Waitlist";
 import About from "../../Components/Landing/About/About";
 import Footer from "../../Components/Landing/Footer/Footer";
 
 export default function Landing() {
-    return (
-        <div>
-            <Navbar />
-            <Hero />
-            <FAQ />
-            <About />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <FAQ />
+      <Waitlist />
+      <About />
+      <Footer />
+    </div>
+  );
 }
