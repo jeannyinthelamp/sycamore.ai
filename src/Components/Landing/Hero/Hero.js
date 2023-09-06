99b14ba11bfaf734c8d6b90cd3ca8a61e7bdd3e6
Akin Evans <akinevans@gmail.com>
2023-09-06 12:05:14 -0700

    Hero Component: Update component height, change Join Waitlist button into input element, update form dimensions

--- src/Components/Landing/Hero/Hero.js
+++ src/Components/Landing/Hero/Hero.js
@@ -2,40 +2,47 @@ import React from "react";
 import Graphic from "./Graphic";
 
 const Hero = () => {
-    return (
-        <div className="bg-[#FAFAFF] font-Poppins">
-            <div className="px-[168px] py-[126px] grid grid-cols-1 lg:grid-cols-2  gap-[142px] h-screen">
-                <div className="mt-24">
-                    <div className="flex flex-col ">
-                        <h1 className="text-[40px] leading-[60px] font-semibold mb-6">
-                            Project Management, Simplified.
-                        </h1>
-                        <p className="mb-8 leading-[27px] font-[18px] ">
-                            Empowering workflows for students, freelancers,
-                            <br /> and small businesses.
-                        </p>
-                        <label class="bg-white w-[440px] rounded-lg border border-solid border-neutral-400 shadow-md">
-                            <div class="flex items-center justify-between py-2 px-4">
-                                <input
-                                    class=" leading-6 text-[16px]"
-                                    type="email"
-                                    required
-                                    placeholder="Email Address"
-                                />
-                                <button class="px-6 py-3 border-[1px] rounded-lg bg-[#556AEB] text-white">
-                                    Join Waitlist
-                                </button>
-                            </div>
-                        </label>
-                    </div>
-                </div>
-
-                <div>
-                    <Graphic />
-                </div>
-            </div>
+  return (
+    <div className='bg-[#FAFAFF] font-Poppins'>
+      <div className='px-[168px] py-[126px] grid grid-cols-1 lg:grid-cols-2  gap-[142px] h-auto min-h-[880px]'>
+        <div className='mt-24'>
+          <div className='flex flex-col'>
+            <h1 className='text-[40px] leading-[60px] font-semibold mb-6'>
+              Project Management, Simplified.
+            </h1>
+            <p className='mb-8 leading-[27px] font-[18px] '>
+              Empowering workflows for students, freelancers,
+              <br /> and small businesses.
+            </p>
+            <form
+              action=''
+              className='flex flex-col w-[440px] bg-white rounded-lg  border border-solid border-neutral-400 shadow-md'
+            >
+              {/* <label className='border border-solid'> */}
+              <div class='flex items-center justify-between py-2 px-4'>
+                <input
+                  className='w-[60%] leading-6 text-[16px] placeholder-[#212529] outline-none'
+                  type='email'
+                  required
+                  placeholder='Email Address'
+                />
+                <input
+                  type='submit'
+                  value='Join Waitlist'
+                  class='w-[146px] h-[48px] px-6 py-3 border-[1px] rounded-lg bg-[#556AEB] text-white cursor-pointer'
+                />
+              </div>
+              {/* </label> */}
+            </form>
+          </div>
         </div>
-    );
+
+        <div>
+          <Graphic />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Hero;
