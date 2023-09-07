fadb249a14fc2e208b26a4f4fcd504acc2c49935
Akin Evans <akinevans@gmail.com>
2023-09-07 10:56:02 -0700

    Begin building form for onboarding page 1

--- src/Pages/Onboarding/Onboarding_1.js
+++ src/Pages/Onboarding/Onboarding_1.js
@@ -6,8 +6,27 @@ import Onboarding_progress_bar from "../../Components/ButtonSmall/Onboarding_pro
 export default function Onboarding_1() {
   return (
     <div>
+      {/* The 'Next' button in header may need to operate as both a submit button for the form and a transition to the next page */}
       <Onboarding_header />
       <Onboarding_nav />
+      <div className='title-and-form-wrapper w-[424px] h-auto mx-auto flex flex-col justify-center items-center border border-solid border-red-500'>
+        <h1 className='mb-[25px] font-[Inter] text-[44px] font-semibold text-black text-center leading-[140%] tracking-[-0.836px]'>
+          Let's set up your account
+        </h1>
+        <form
+          action=''
+          className='w-[424px] h-auto flex flex-col justify-center items-start border border-solid border-blue-500'
+        >
+          <label className='mb-[10px] flex flex-col font-[Inter] text-[#C8C8C8] text-sm font-medium leading-[21px] tracking-[-0.266px]'>
+            Your Name
+            <input type='text' name='name' />
+          </label>
+          <label className='mb-[10px] flex flex-col font-[Inter] text-[#C8C8C8] text-sm font-medium leading-[21px] tracking-[-0.266px]'>
+            Email
+            <input type='text' name='name' />
+          </label>
+        </form>
+      </div>
       {/* Change the active prop to page2, page3 etc to change the color of the corresponding bar */}
       <Onboarding_progress_bar active='page1' />
     </div>
