94091329debdcf6d22751ee333b1ee1b4123800b
Akin Evans <akinevans@gmail.com>
2023-09-08 12:39:32 -0700

    Update Onboarding progress bar component so that each bar also acts as a link to its respective page

--- src/Components/ButtonSmall/Onboarding_progress_bar/Onboarding_progress_bar.js
+++ src/Components/ButtonSmall/Onboarding_progress_bar/Onboarding_progress_bar.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 export default function Onboarding_progress_bar(props) {
   const active_style = "w-[127px] h-[8px] mx-[7px] rounded-[100px] bg-black";
   const non_active_style =
@@ -7,36 +7,53 @@ export default function Onboarding_progress_bar(props) {
 
   return (
     <div className='w-[100%] h-auto mt-[140px] flex flex-row justify-center items-center '>
-      <div
-        className={`ml-0 ${
-          props.active === "page1" ? active_style : non_active_style
-        }`}
-      ></div>
-      <div
-        className={`${
-          props.active === "page2" ? active_style : non_active_style
-        }`}
-      ></div>
-      <div
-        className={`${
-          props.active === "page3" ? active_style : non_active_style
-        }`}
-      ></div>
-      <div
-        className={`${
-          props.active === "page4" ? active_style : non_active_style
-        }`}
-      ></div>
-      <div
-        className={`${
-          props.active === "page5" ? active_style : non_active_style
-        }`}
-      ></div>
-      <div
-        className={`mr-0 ${
-          props.active === "page6" ? active_style : non_active_style
-        }`}
-      ></div>
+      <Link to='/onboarding_1'>
+        <div
+          className={`ml-0 ${
+            props.active === "page1" ? active_style : non_active_style
+          }`}
+        ></div>
+      </Link>
+
+      <Link to='/onboarding_2'>
+        <div
+          className={`${
+            props.active === "page2" ? active_style : non_active_style
+          }`}
+        ></div>
+      </Link>
+
+      <Link to='/onboarding_3'>
+        <div
+          className={`${
+            props.active === "page3" ? active_style : non_active_style
+          }`}
+        ></div>
+      </Link>
+
+      <Link to='/onboarding_4'>
+        <div
+          className={`${
+            props.active === "page4" ? active_style : non_active_style
+          }`}
+        ></div>
+      </Link>
+
+      <Link to='/onboarding_5'>
+        <div
+          className={`${
+            props.active === "page5" ? active_style : non_active_style
+          }`}
+        ></div>
+      </Link>
+
+      <Link to='/onboarding_6'>
+        <div
+          className={`mr-0 ${
+            props.active === "page6" ? active_style : non_active_style
+          }`}
+        ></div>
+      </Link>
     </div>
   );
 }
