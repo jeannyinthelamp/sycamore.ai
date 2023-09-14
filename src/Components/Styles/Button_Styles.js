550997fcc557474d58bc7692095c3355d1e175b8
Akin Evans <akinevans@gmail.com>
2023-09-14 13:19:31 -0700

    Build custom styles for button hover and active states - located in Button_Styles.js

---
+++ src/Components/Styles/Button_Styles.js
@@ -0,0 +1,22 @@
+// export const btn_blue_hover = "hover:bg-[#1D2F99]";
+// export const btn_blue_active = "";
+
+// export const btn_white_hover = "";
+// export const btn_white_active = "";
+
+export const btn_styles = {
+  btn_blue_hover:
+    "ease-in-out duration-[200ms] hover:bg-[#1D2F99] hover:border-[#1D2F99]",
+  //
+  btn_blue_active: "active:bg-[#0C1A66]",
+  //
+  btn_white_hover:
+    "ease-in-out duration-[200ms] hover:text-[#1D2F99] hover:border-[#1D2F99]",
+  //
+  btn_white_active: " active:text-[#0C1A66] active:border-[#0C1A66]",
+  //
+  focus: "outline-none",
+  //
+  disable:
+    "text-[#6C757D] bg-[#E9ECEF]  border-none hover:bg-[#E9ECEF] hover:text-[#6C757D] active:bg-[#E9ECEF] active:text-[#6C757D] cursor-not-allowed",
+};
