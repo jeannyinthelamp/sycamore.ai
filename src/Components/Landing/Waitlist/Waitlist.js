ce58f2f17959c0e60cc0a8d8952fdd99f4cd395e
Akin Evans <akinevans@gmail.com>
2023-09-19 13:34:05 -0700

    Build Waitlist mobile version

--- src/Components/Landing/Waitlist/Waitlist.js
+++ src/Components/Landing/Waitlist/Waitlist.js
@@ -18,18 +18,21 @@ export default function Waitlist() {
       className='waitlist-outer-wrapper desktop:max-w-[100%] h-auto flex justify-center items-center bg-[#FFF]
     
     tablet:max-w-[834px] tablet:max-h-[786px]
+    
     '
     >
       <div
         className='waitlist-inner-wrapper relative desktop:w-[1440px] h-[800px] flex justify-center items-center
       
         tablet:max-w-[100%]
+        mobile:max-w-[100%]
       '
       >
         <div
           className='image-wrapper relative w-[100%] desktop:max-w-[500px] desktop:h-[552px] z-0
         
         tablet:max-w-[519px] 
+        mobile:max-w-[338px] 
         '
         >
           {/* //* images here */}
@@ -40,6 +43,8 @@ export default function Waitlist() {
             className='absolute z-[-1] desktop:top-[4%] desktop:left-[-56%] 
             
             tablet:w-[70%] tablet:top-[-15%] tablet:left-[-40%]
+
+            mobile:w-[70%] mobile:top-[-15%] mobile:left-[12%]
             '
           />
           <img
@@ -49,24 +54,32 @@ export default function Waitlist() {
             className='absolute z-[-1] desktop:bottom-[5%] desktop:right-[-43%]
             
             tablet:w-[32%] tablet:bottom-[-3%] tablet:right-[-24.5%]
+
+            mobile:hidden
             '
           />
           <div
             className='waitlist-card-wrapper z-10 w-[100%] desktop:max-w-[500px] desktop:h-[552px] flex flex-col desktop:justify-start items-center desktop:p-[50px] bg-white border border-solid border-[#CED4DA] rounded-[20px]
           
            tablet:max-w-[519px] tablet:h-[699px] tablet:justify-start tablet:py-[50px] tablet:px-[16px]
+
+           mobile:max-w-[338px] mobile:h-fit mobile:py-[50px] mobile:px-[16px]
           '
           >
             <div
               className='waitlist-card-text-wrapper desktop:max-w-[298px] h-auto flex flex-col gap-[16px] items-center
             
             tablet:max-w-[317px]
+
+            mobile:max-w-[298px]
             '
             >
               <h1
-                className='font-Poppins desktop:text-[28px] font-semibold leading-[40px]
+                className='font-Poppins desktop:text-[28px] font-semibold leading-[40px] text-center
               
               tablet:text-[38px] tablet:leading-[51px]
+
+              mobile:text-[28px] mobile:leading-[40px]
               '
               >
                 Join our Waitlist
@@ -75,6 +88,8 @@ export default function Waitlist() {
                 className='font-Poppins desktop:text-[14px] text-[#212529] font-normal desktop:leading-[21px] text-center
               
                 tablet:w-[95%] tablet:text-[18px] tablet:leading-[27px]
+
+                mobile:w-[97%] mobile:text-[16px] mobile:leading-[24px]
               '
               >
                 Join the waitlist for exclusive access to Syne. Don't miss out
@@ -86,12 +101,16 @@ export default function Waitlist() {
               className='waitlist-form-wrapper desktop:w-auto mt-[32px] flex flex-col justify-start items-center
             
             tablet:max-w-[100%] tablet:w-[487px]
+
+            mobile:max-w-[100%] mobile:w-[306px]
             '
             >
               <div
                 className='first-last-names-wrapper w-[100%] flex desktop:flex-row
               
               tablet:flex-col tablet:gap-[14px] 
+
+              mobile:flex-col mobile:gap-[14px] 
               '
               >
                 <label
@@ -106,6 +125,8 @@ export default function Waitlist() {
                     className={`${detectBrowser()} desktop:w-[191px] h-auto mt-[8px] mr-[17px] py-[10px] px-[16px] font-Inter text-[#212529] outline outline-1 outline-[#CED4DA] rounded-lg shadow-[0_2px_2px_0_rgba(33,37,41,0.08)]
                     
                     tablet:w-[100%]
+
+                    mobile:w-[100%]
                     `}
                   />
                 </label>
@@ -118,6 +139,9 @@ export default function Waitlist() {
                     className={`${detectBrowser()} w-[191px] h-auto mt-[8px] py-[10px] px-[16px] font-Inter text-[#212529] outline outline-1 outline-[#CED4DA] rounded-lg shadow-[0_2px_2px_0_rgba(33,37,41,0.08)]
                     
                     tablet:w-[100%]
+
+                    mobile:w-[100%]
+
                     `}
                   />
                 </label>
@@ -127,12 +151,15 @@ export default function Waitlist() {
                 className='form-email-button-wrapper desktop:w-[100%] h-auto desktop:mt-[32px] flex flex-col desktop:justify-center desktop:items-center
               
               tablet:w-[100%] tablet:justify-center tablet:items-start tablet:mt-[14px]
+
+              mobile:w-[100%] mobile:mt-[14px]
               '
               >
                 <label
                   className='flex flex-col font-Poppins text-[#6C757D] text-[14px] font-normal leading-[21px]
                 
                 tablet:w-[100%]
+                mobile:w-[100%]
                 '
                 >
                   Email
@@ -143,8 +170,9 @@ export default function Waitlist() {
                     required
                     className={`${detectBrowser()} desktop:w-[400px] h-auto mt-[8px] py-[10px] px-[16px] font-Inter text-[#212529] outline outline-1 outline-[#CED4DA] rounded-lg shadow-[0_2px_2px_0_rgba(33,37,41,0.08)]
                     
-                    
                     tablet:w-[100%]
+
+                    mobile:w-[100%]
                     `}
                   />
                 </label>
@@ -153,6 +181,7 @@ export default function Waitlist() {
                   className='toggle-wrapper mt-[33.5px] w-[100%] flex flex-row desktop:justify-start desktop:items-center 
                 
                 tablet:justify-center tablet:items-center
+                mobile:justify-center mobile:items-center
                 '
                 >
                   {/* //* Build Toggle Switch here */}
@@ -179,6 +208,8 @@ export default function Waitlist() {
                   className={`${btn_styles.btn_blue_hover} ${btn_styles.btn_blue_active} desktop:w-[400px] mt-[32px] py-[12px] px-[24px] flex justify-center items-center bg-[#556AEB] rounded-lg font-Poppins text-[#FFF] text-center text-[16px] font-medium leading-[24px] outline-none border-none cursor-pointer ${btn_styles.focus}
                   
                   tablet:w-[146px] tablet:mx-auto
+
+                  mobile:w-[146px] mobile:mx-auto
                   `}
                 />
               </div>
