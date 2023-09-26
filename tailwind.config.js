2b4970de1cf36429686933f9d9144ca7abd71534
Akin Evans <akinevans@gmail.com>
2023-09-26 11:24:34 -0700

    add screen breakpoints to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,20 +6,25 @@ const contentArray = ["./src/**/*.{js,jsx,ts,tsx}"];
 
 // Merging the configurations
 module.exports = withMT({
-    content: contentArray,
-    theme: {
-        extend: {
-            fontFamily: {
-                Rubik: ["Rubik"],
-                Gloria: ["Gloria Hallelujah"],
-                Poppins: ["Poppins", "sans-serif"],
-                Inter: ["Inter", "sans-serif"],
-            },
-            colors: {
-                "base-black": "#424242",
-            },
-            // Other theme extensions can be added here
-        },
+  content: contentArray,
+  theme: {
+    extend: {
+      fontFamily: {
+        Rubik: ["Rubik"],
+        Gloria: ["Gloria Hallelujah"],
+        Poppins: ["Poppins", "sans-serif"],
+        Inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        "base-black": "#424242",
+      },
+      // Other theme extensions can be added here
     },
-    plugins: [],
+    screens: {
+      desktop: { min: "835px", max: "10000px" },
+      tablet: { min: "376px", max: "834px" },
+      mobile: { min: "1px", max: "375px" },
+    },
+  },
+  plugins: [],
 });
