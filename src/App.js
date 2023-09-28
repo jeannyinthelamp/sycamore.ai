6a2155251e3068e7bb5acc903ef38c80951cd9be
Akin Evans <akinevans@gmail.com>
2023-09-28 15:12:07 -0700

    Update Routes in App.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import Loggingin from "./Pages/Login/Loggingin";
 import Signup from "./Pages/Signup/Signup";
 import Signup2 from "./Pages/Signup2/Signup2";
 import Onboarding_1 from "./Pages/Onboarding/Onboarding_1";
+import Onboarding_2 from "./Pages/Onboarding/Onboarding_2";
 import Onboarding_3 from "./Pages/Onboarding/Onboarding_3";
+import Onboarding_4 from "./Pages/Onboarding/Onboarding_4";
 import Onboarding_5 from "./Pages/Onboarding/Onboarding_5";
+import Onboarding_6 from "./Pages/Onboarding/Onboarding_6";
+import Onboarding_7 from "./Pages/Onboarding/Onboarding_7";
 import ErrorPage from "./Pages/ErrorPage/ErrorPage";
 import "./index.css";
 
@@ -17,8 +21,12 @@ export default function App() {
       <Route path='/sign-up' element={<Signup />} />
       <Route path='/sign-up-2' element={<Signup2 />} />
       <Route path='/onboarding_1' element={<Onboarding_1 />} />
+      <Route path='/onboarding_2' element={<Onboarding_2 />} />
       <Route path='/onboarding_3' element={<Onboarding_3 />} />
+      <Route path='/onboarding_4' element={<Onboarding_4 />} />
       <Route path='/onboarding_5' element={<Onboarding_5 />} />
+      <Route path='/onboarding_6' element={<Onboarding_6 />} />
+      <Route path='/onboarding_7' element={<Onboarding_7 />} />
       <Route path='*' element={<ErrorPage />} />
     </Routes>
   );
