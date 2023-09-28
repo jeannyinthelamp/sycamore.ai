61e3b8ffd6d65f8f71ff175d5fee59ceab9d343d
Akin Evans <akinevans@gmail.com>
2023-09-28 15:24:43 -0700

    Delete Onboarding Back Button component, update letter spacing

--- src/Components/Onboarding/Onboarding_header/Onboarding_header.js
+++ src/Components/Onboarding/Onboarding_header/Onboarding_header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import syne_logo_blue from "../../../Assets/Icons/syne-logo-blue.svg";
+import syne_logo from "../../../Assets/Icons/syne-logo.svg";
 
 export default function Onboarding_header() {
   return (
@@ -7,7 +7,7 @@ export default function Onboarding_header() {
       <div className='header-inner-wrapper w-[100%] max-w-[1110px] h-[40px] mx-[20px] flex justify-start items-center'>
         <img
           className='w-[30px] h-[30px] mr-[14px]'
-          src={syne_logo_blue}
+          src={syne_logo}
           alt='Syne Logo'
         />
         <p className='font-Poppins text-[20px] font-normal leading-[30px] tracking-[-0.266px]'>
