5d90d8e0d9650c5cacab3eeeb22d0bd76f451e91
Akin Evans <akinevans@gmail.com>
2023-10-10 11:05:57 -0700

    Update UI writing for Onboarding page 7

--- src/Pages/Onboarding/Onboarding_7.js
+++ src/Pages/Onboarding/Onboarding_7.js
@@ -4,8 +4,6 @@ import checkmark from "../../Assets/Icons/check_circle.svg";
 import { Link } from "react-router-dom";
 
 export default function Onboarding_7() {
-  //update username
-  let userName = undefined;
   return (
     <div className='flex flex-col justify-center items-center'>
       <Onboarding_Header />
@@ -13,13 +11,16 @@ export default function Onboarding_7() {
         <img
           src={checkmark}
           alt='check mark'
-          className='max-w-[95px] mb-[60.5px]'
+          className='max-w-[95px] mb-[48px]'
         />
+        <h1 className='mb-[24px] font-Poppins text-[40px] font-semibold leading-[54px] text-center'>
+          Your workspace has been successfully created!
+        </h1>
         <p className='w-auto h-auto mb-[72px] font-Poppins text-[18px] font-medium text-[#212529] leading-[28px] text-center'>
-          {`Nice! ${userName} has been successfully created in your workspace!`}
+          Nice! Go to dashboard to see your newly created project.
         </p>
-        <Link to='' className='w-[100%] h-auto'>
-          <button className='w-[100%] py-[12px]  flex justify-center items-center bg-[#212529] rounded-lg font-Poppins text-[#F8F9FA] text-center text-[16px] font-medium leading-[24px] outline-none border-none cursor-pointer'>
+        <Link to='' className='w-[100%] h-auto flex justify-center'>
+          <button className='w-[72.6%] py-[12px]  flex justify-center items-center bg-[#212529] rounded-lg font-Poppins text-[#F8F9FA] text-center text-[16px] font-medium leading-[24px] outline-none border-none cursor-pointer'>
             Go to Dashboard
           </button>
         </Link>
