5620dedb12362b77c3af8d66716f5687412f7a22
Akin Evans <akinevans@gmail.com>
2023-10-31 13:39:22 -0700

    Update Onboarding_Header function name

--- src/Components/Onboarding/Onboarding_header/Onboarding_header.js
+++ src/Components/Onboarding/Onboarding_header/Onboarding_header.js
@@ -1,19 +1,19 @@
 import React from "react";
 import syne_logo from "../../../Assets/Icons/syne-logo.svg";
 
-export default function Onboarding_header() {
-    return (
-        <div className="header-outer-wrapper w-[100%] h-[72px] flex justify-center items-center">
-            <div className="header-inner-wrapper w-[100%] max-w-[1110px] h-[40px] mx-[20px] flex justify-start items-center">
-                <img
-                    className="w-[30px] h-[30px] mr-[14px]"
-                    src={syne_logo}
-                    alt="Syne Logo"
-                />
-                <p className="font-Poppins text-[20px] font-normal leading-[30px] tracking-[-0.266px]">
-                    Syne
-                </p>
-            </div>
-        </div>
-    );
+export default function Onboarding_Header() {
+  return (
+    <div className='header-outer-wrapper w-[100%] h-[72px] flex justify-center items-center'>
+      <div className='header-inner-wrapper w-[100%] max-w-[1110px] h-[40px] mx-[20px] flex justify-start items-center'>
+        <img
+          className='w-[30px] h-[30px] mr-[14px]'
+          src={syne_logo}
+          alt='Syne Logo'
+        />
+        <p className='font-Poppins text-[20px] font-normal leading-[30px] tracking-[-0.266px]'>
+          Syne
+        </p>
+      </div>
+    </div>
+  );
 }
