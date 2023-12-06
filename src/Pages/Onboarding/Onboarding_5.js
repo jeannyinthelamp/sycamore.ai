86bcca06bab1c3119f50e8bedf1b1866e84b9ace
Karina Song <ksysong@gmail.com>
2023-12-05 22:04:23 -0800

    update onboarding 5 to match new design

--- src/Pages/Onboarding/Onboarding_5.js
+++ src/Pages/Onboarding/Onboarding_5.js
@@ -1,13 +1,20 @@
 /* eslint-disable react/jsx-pascal-case */
-import React from "react";
+import React, { useState } from "react";
 import Onboarding_header from "../../Components/Onboarding/Onboarding_header/Onboarding_header";
 import Onboarding_progress_bar from "../../Components/Onboarding/Onboarding_progress_bar/Onboarding_progress_bar";
 import Onboarding_nav from "../../Components/Onboarding/Onboarding_nav/Onboarding_nav";
 import { Link } from "react-router-dom";
+import shareLink from "../../Assets/Icons/Onboarding_Icons/link.svg"
+import info from "../../Assets/Icons/Onboarding_Icons/info.svg"
+import Onboarding_Skip_Continue_Btns from "../../Components/Onboarding/Onboarding_Skip_Continue_Buttons/Onboarding_Skip_Continue_Buttons";
+import Onboarding_Alert_Modal from "../../Components/Onboarding/Onboarding_Alert_Modal/Onboarding_Alert_Modal";
 import { safari_input_styling } from "../../Components/Styles/Safari_Input_Styling";
 
 export default function Onboarding_5() {
   // Safari has an issue where its difficult to change input border-radius. This function detects a users browser, then injects classNames into create an outline
+
+  const [showLinkAlert, setShowLinkAlert] = useState(false); 
+
   function detectBrowser() {
     if (navigator.userAgent.includes("Safari")) {
       return safari_input_styling;
@@ -16,105 +23,60 @@ export default function Onboarding_5() {
   return (
     <div className='flex flex-col justify-center items-center'>
       <Onboarding_header />
-
-      {/* Change the active prop to page2, page3 etc to change the color of the corresponding bar */}
-      <Onboarding_progress_bar active='page5' />
-
-      <div className='title-and-form-wrapper max-w-[541px] mx-[20px] h-auto flex flex-col justify-center items-center'>
-        <div className='text-wrapper max-w-[541px] h-auto mb-[72px] flex flex-col justify-start items-center'>
+      <Onboarding_nav />
+      <div className='title-and-form-wrapper max-w-[730px] mx-[20px] h-auto flex flex-col justify-center items-center'>
+        <div className='text-wrapper max-w-[730px] h-auto mb-[48px] flex flex-col justify-start items-center'>
           <h1 className='mb-[24px] mx-[22px] font-Poppins text-[40px] font-semibold text-black text-center leading-[54px]'>
-            Let's create a project
+            Invite your team members
           </h1>
           <p className='font-Poppins text-[18px] font-medium leading-[28px] text-center'>
-            Add information about your project
+            Add members to collaborate within a workspace. Type or paste in one or multiple emails separated by commas. 
           </p>
         </div>
         <form
           action=''
-          className='w-[79.8%] h-auto flex flex-col gap-[24px] justify-center items-start'
+          className='w-full max-w-[420px] h-auto flex flex-col gap-[24px] justify-center items-start gap-y-[24px]'
         >
-          <label
-            className='w-[100%] flex flex-col gap-[5px] font-Poppins text-[#00000080] text-[16px] font-normal leading-[24px]
-                '
+          <label hidden="hidden" for="emails">Emails:</label>
+          <textarea
+            className={`${detectBrowser()}block w-full h-[110px] border py-[10px] px-[14px] border-[#ADB5BD] rounded-lg resize-none`}
+            placeholder="johndoe@gmail.com, janedoe@gmail.com, ..."
+            name="emails"
           >
-            Project Title *
-            <input
-              //
-              type='text'
-              name='project-title'
-              required
-              className={`${detectBrowser()} w-[100%] h-auto py-[10px] px-[16px] font-Poppins font-normal text-[#212529] leading-[24px] outline outline-1 outline-[#CED4DA] rounded-lg`}
-            />
-          </label>
 
-          <label
-            className='w-[100%] flex flex-col gap-[5px] font-Poppins text-[#00000080] text-[16px] font-normal leading-[24px] 
-                '
+          </textarea>
+          <button
+            className="text-[#0A70E8] flex self-end"
+            onClick={(e) => {
+              e.preventDefault();
+              setShowLinkAlert(true); // trigger is here to test the show link success modal, remove later when share Link confirmation modal is added
+            }}
           >
-            Date *
-            <input
-              //
-              type='date'
-              name='date'
-              required
-              className={`${detectBrowser()} w-[100%] h-auto py-[10px] px-[16px] font-Poppins font-normal text-[#212529] leading-[24px]  outline outline-1 outline-[#CED4DA] rounded-lg`}
-            />
-          </label>
-
-          <div className='status-priority-wrapper flex flex-row gap-[20px]'>
-            <label
-              className='w-[100%] flex flex-col gap-[5px] font-Poppins text-[#00000080] text-[16px] font-normal leading-[24px] 
-                '
-            >
-              Status *
-              <input
-                //
-                type='text'
-                name='status'
-                placeholder='Not Selected'
-                required
-                className={`${detectBrowser()} w-[100%] h-auto py-[10px] px-[16px] font-Poppins font-normal text-[#212529] leading-[24px]  outline outline-1 outline-[#CED4DA] rounded-lg`}
-              />
-            </label>
-
-            <label
-              className='w-[100%] flex flex-col gap-[5px] font-Poppins text-[#00000080] text-[16px] font-normal leading-[24px] 
-                '
-            >
-              Priority *
-              <input
-                //
-                type='text'
-                name='priority'
-                placeholder='Not Selected'
-                required
-                className={`${detectBrowser()} w-[100%] h-auto py-[10px] px-[16px] font-Poppins font-normal text-[#212529] leading-[24px]  outline outline-1 outline-[#CED4DA] rounded-lg`}
-              />
-            </label>
-          </div>
-          <label
-            className='w-[100%] mb-[48px] flex flex-col gap-[5px] font-Poppins text-[#00000080] text-[16px] font-normal leading-[24px] 
-                '
-          >
-            Category (Optional)
-            <input
-              //
-              type='text'
-              name='project-title'
-              placeholder='Not Selected'
-              className={`${detectBrowser()} w-[100%] h-auto py-[10px] px-[16px] font-Poppins font-normal text-[#212529] leading-[24px] outline outline-1 outline-[#CED4DA] rounded-lg`}
-            />
-          </label>
-
-          {/* //* Navigation Button and form Submit Input */}
-          <Onboarding_nav />
+            <img src={shareLink} alt="Share Link Icon" className="mr-[4px]" />
+            Share Link
+          </button>
+          <Onboarding_Skip_Continue_Btns 
+            skipToPage=""
+            formId=""
+            btnText="Invite and take me to Syne"
+            disabled={false}
+            message="Are you sure you want to skip?"
+          />
         </form>
-        <Link
-          to=''
-          className='h-[24px] mt-[12px] font-Poppins font-medium text-[16px] leading-[24px] text-[#556AEB] text-center underline underline-offset-2 cursor-pointer'
-        >
-          Ask Later
-        </Link>
+          {/* //* Navigation Button and form Submit Input */}
+      {/* Change the active prop to page2, page3 etc to change the color of the corresponding bar */}
+      <Onboarding_progress_bar active='page5' />
+      <Onboarding_Alert_Modal 
+        isVisible={showLinkAlert ? "visible" : "hidden"}
+        modalPosition='top-[140px]'
+        message='Link copied!'
+        border='border-[#237B4B]'
+        background='bg-[#C1FFDD]'
+        iconLeft={info}
+        iconLeftAlt="Info Icon"
+        skipBtnVisible={false}
+        exitBtnOnClick={() => setShowLinkAlert(false)}
+      />
       </div>
     </div>
   );
