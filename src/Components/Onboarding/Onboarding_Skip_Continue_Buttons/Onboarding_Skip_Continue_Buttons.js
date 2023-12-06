32fca1197e2d763ae9283480c6ffd0bdfa87a5ed
Karina Song <ksysong@gmail.com>
2023-12-05 22:23:44 -0800

    update skip cont btns component to take skip msg prop

--- src/Components/Onboarding/Onboarding_Skip_Continue_Buttons/Onboarding_Skip_Continue_Buttons.js
+++ src/Components/Onboarding/Onboarding_Skip_Continue_Buttons/Onboarding_Skip_Continue_Buttons.js
@@ -9,7 +9,7 @@ import warning from "../../../Assets/Icons/Onboarding_Icons/warning.svg";
 //   2) pass the id of the form (formID)
 //   3) pass the button text of the Continue Btn (btnText)
 //   4) pass the state of the Continue Button (disabledState will be true or false)
-//   5) pass Alert Message (message)
+//   5) pass skip message (skipMessage)
 
 // Issues: Modal doesn't appear at top of the page
 
@@ -38,7 +38,7 @@ export default function Onboarding_Skip_Continue_Btns(props) {
         }}
         className='text-[#0A70E8] underline my-[24px]'
       >
-        Skip
+        {props.skipMessage ? props.skipMessage : "Skip"}
       </button>
       <OnboardingAlertModal
         isVisible={showAlert ? "visible" : "hidden"}
