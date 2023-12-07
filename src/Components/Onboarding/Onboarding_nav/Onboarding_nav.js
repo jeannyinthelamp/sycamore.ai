404404bbc8f8511dfcb14ca329ee52c8812742d4
Cera <cerasamson@gmail.com>
2023-12-07 12:25:33 -0800

    Change color on back.svg hover

--- src/Components/Onboarding/Onboarding_nav/Onboarding_nav.js
+++ src/Components/Onboarding/Onboarding_nav/Onboarding_nav.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 import back from "../../../Assets/Icons/Onboarding_Icons/back.svg"
+import back_selected from "../../../Assets/Icons/Onboarding_Icons/back_selected.svg"
 import exit from "../../../Assets/Icons/Onboarding_Icons/exit.svg"
 
 
 export default function Onboarding_Nav() {
   const currentLocation = useLocation();
+
+  const [isHovered, setIsHovered] = useState(false);
 
   //Back button functionality
   const traverseBack = (currentPath) => {
@@ -31,25 +34,6 @@ export default function Onboarding_Nav() {
     }
   };
 
-  //Continue button functionality
-  const traverseNext = (currentPath) => {
-    switch (currentPath) {
-      case "/onboarding_1":
-        return "/onboarding_2";
-
-      case "/onboarding_2":
-        return "/onboarding_3";
-
-      case "/onboarding_3":
-        return "/onboarding_4";
-
-      case "/onboarding_4":
-        return "/onboarding_5";
-
-      default:
-        return;
-    }
-  };
   return (
     //* Back and Exit buttons
 <div class="flex p-4 md:p-16 justify-between items-center self-stretch">
@@ -68,16 +52,19 @@ export default function Onboarding_Nav() {
           );
         }}
       >
-      <div class="flex items-center hover:text-[#556AEB]">
-        <img
-          class='mr-2'
-          src={back}
-          alt='back'
-        />  
-        Back
-      </div>
+        <div class="flex items-center hover:text-[#556AEB]"
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)} >
+          <img
+            class='mr-2'
+            src={isHovered ? back_selected : back}
+            alt='back'
+          />  
+          Back
+        </div>
       </Link> 
-      <button 
+      
+    <button 
     className='flex'>
       <img
           className=''
@@ -85,7 +72,6 @@ export default function Onboarding_Nav() {
           alt='exit'
         />
         </button>
-      {/* Account for Onboarding1 - where there is no back button */}
     </div>
   );
 }
